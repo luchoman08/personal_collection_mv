7af89805aed657765c249517913b13a4d1a5681a
Lgmc <luis.manrique@correounivalle.edu.co>
2016-05-02 00:19:57 +0000

    eliminacion de todo

--- personal_collection_movies/log_bd/psql_proyecto.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXIST usuarios on cascade;
-create table usuarios(id integer primary key AI,
-num_identificacion varchar(50) unique,
-nombres varchar(50) not null,
-correo_electronico varchar(50));
