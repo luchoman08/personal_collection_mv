5d535dd99bfefe11436b00db0939b7e4bf27f004
jeisoncg <jeisoncardona@outlook.com>
2016-05-01 21:44:59 -0500

    Entidad de la tabla usuarios generada, CRUD de usuarios generado

---
+++ personal_collection_movies/src/TinkerSoft/UsuariosBundle/Controller/UsuariosController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace TinkerSoft\UsuariosBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use TinkerSoft\UsuariosBundle\Entity\Usuarios;
+use TinkerSoft\UsuariosBundle\Form\UsuariosType;
+
+/**
+ * Usuarios controller.
+ *
+ */
+class UsuariosController extends Controller
+{
+    /**
+     * Lists all Usuarios entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $usuarios = $em->getRepository('TinkerSoftUsuariosBundle:Usuarios')->findAll();
+
+        return $this->render('usuarios/index.html.twig', array(
+            'usuarios' => $usuarios,
+        ));
+    }
+
+    /**
+     * Creates a new Usuarios entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $usuario = new Usuarios();
+        $form = $this->createForm('TinkerSoft\UsuariosBundle\Form\UsuariosType', $usuario);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($usuario);
+            $em->flush();
+
+            return $this->redirectToRoute('usuarios_show', array('id' => $usuario->getId()));
+        }
+
+        return $this->render('usuarios/new.html.twig', array(
+            'usuario' => $usuario,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Usuarios entity.
+     *
+     */
+    public function showAction(Usuarios $usuario)
+    {
+        $deleteForm = $this->createDeleteForm($usuario);
+
+        return $this->render('usuarios/show.html.twig', array(
+            'usuario' => $usuario,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Usuarios entity.
+     *
+     */
+    public function editAction(Request $request, Usuarios $usuario)
+    {
+        $deleteForm = $this->createDeleteForm($usuario);
+        $editForm = $this->createForm('TinkerSoft\UsuariosBundle\Form\UsuariosType', $usuario);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($usuario);
+            $em->flush();
+
+            return $this->redirectToRoute('usuarios_edit', array('id' => $usuario->getId()));
+        }
+
+        return $this->render('usuarios/edit.html.twig', array(
+            'usuario' => $usuario,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Usuarios entity.
+     *
+     */
+    public function deleteAction(Request $request, Usuarios $usuario)
+    {
+        $form = $this->createDeleteForm($usuario);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($usuario);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('usuarios_index');
+    }
+
+    /**
+     * Creates a form to delete a Usuarios entity.
+     *
+     * @param Usuarios $usuario The Usuarios entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Usuarios $usuario)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('usuarios_delete', array('id' => $usuario->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ personal_collection_movies/src/TinkerSoft/UsuariosBundle/Form/UsuariosType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TinkerSoft\UsuariosBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuariosType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellidos')
+            ->add('nickname')
+            ->add('correoElectronico')
+            ->add('password')
+            ->add('rol')
+            ->add('estado')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TinkerSoft\UsuariosBundle\Entity\Usuarios'
+        ));
+    }
+}
